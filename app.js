a7548e974f7620a67743a073c71aba76360dcddd
SandeshKumar503 <Sandesh.suther@gmail.com>
2025-06-18 12:25:36 +0500

    Final update

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
+let count = 0;
 
+function changeNumber() {
   document.getElementById("count").textContent = count;
-  let counter = document.getElementById("counter");
+  const counter = document.getElementById("counter");
 
-
-  function increaseValue = {
-    if
+  if (count === 0) {
+    counter.textContent = "ZERO";
+  } else if (count > 0 && count <= 20) {
+    counter.textContent = count.toString();
   }
+}
+
+function increase() {
+  if (count < 20) {
+    count++;
+    changeNumber();
+  }
+}
+
+function decrease() {
+  if (count > 0) {
+    count--;                
+    changeNumber();
+  }
+}
+
